383067759e168bfc888dc441d1b70ae3494ed8b6
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-05 03:15:43 +0530

    BST find contains

---
+++ BinarySearchTrees/3-BST-find-contains.js
@@ -0,0 +1,97 @@
+/* 
+Starting at the root
+    Check if there is a root, if not - we're done searching!
+    If there is a root, check if the value of the new node is the value we are looking for. If we found it, we're done!
+    If not, check to see if the value is greater than or less than the value of the root
+    If it is greater 
+        Check to see if there is a node to the right
+            If there is, move to that node and repeat these steps
+            If there is not, we're done searching!
+    If it is less
+        Check to see if there is a node to the left
+            If there is, move to that node and repeat these steps
+            If there is not, we're done searching!
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  find(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+
+  contains(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+console.log(tree.find(7)); // Node { value: 7, left: null, right: null }
+console.log(tree.contains(7)); // true
