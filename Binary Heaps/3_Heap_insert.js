e2aa49dfdc62cc6edb93c6fa0a4cd075b27f1219
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-17 22:51:55 +0530

    heap insert

--- Binary Heaps/3_Heap_insert.js
+++ Binary Heaps/3_Heap_insert.js
@@ -20,3 +20,30 @@ Bubble UP:
 
 
 */
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [41, 39, 33, 18, 27, 12];
+  }
+
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp(element);
+  }
+
+  bubbleUp(element) {
+    console.log(element);
+    let idx = this.values.length - 1;
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+}
+
+let heap = new MaxBinaryHeap();
+heap.insert(55);
