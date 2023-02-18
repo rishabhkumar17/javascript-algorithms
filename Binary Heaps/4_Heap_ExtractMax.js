7e54778266c8b641d0ff2985666a65dcb082108a
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-18 23:33:11 +0530

    sinkDown func

--- Binary Heaps/4_Heap_ExtractMax.js
+++ Binary Heaps/4_Heap_ExtractMax.js
@@ -16,8 +16,41 @@ Have the new root "sink down" to the correct spot..
 extractMax() {
     const max = this.values[0];
     const end = this.values.pop();
-    this.values[0] = end;
-    // sink down
-    this.sinkDown();
+    if(this.values.length > 0) {
+        this.values[0] = end;
+        this.sinkDown();
+    }
     return max;
+}
+// [33, 39, 41, 18, 27, 12]
+sinkDown() {
+    let idx = 0; 
+    const length = this.values.length;
+    const element = this.values[0];
+    while(true) {
+        let leftChildIdx = 2 * idx + 1;
+        let rightChildIdx = 2 * idx + 2;
+        let leftChild, rightChild;
+        let swap = null;
+
+        if(leftChildIdx < length) {
+            leftChild = this.values[leftChildIdx];
+            if(leftChild > element) {
+                swap = leftChildIdx;
+            }
+        }
+        if(rightChildIdx < length) {
+            rightChild = this.values[rightChildIdx];
+            if(
+                (swap === null && rightChild > element) ||
+                (swap !== null && rightChild > leftChild)
+            ) {
+                swap = rightChildIdx;
+            }
+        }
+        if(swap === null) break;
+        this.values[idx] = this.values[swap];
+        this.values[swap] = element;
+        idx = swap;
+    }
 }
