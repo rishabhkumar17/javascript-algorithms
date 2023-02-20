5879f41132901c6087465fb897ca3ff583e347df
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-20 18:27:16 +0530

    hash tables intro

---
+++ Hash Tables/Hash_Tables.js
@@ -0,0 +1,20 @@
+// What is a HASH TABLE
+
+/* 
+Hash tables are used to store key-value pairs.
+They are like arrays, but the keys are not ordered.
+Unlike arrays, hash tables are fast for all of the following operations:
+finding values, adding new values, and removing values.
+
+Why to use?
+Nearly every programming language has some sort of hash table data
+    structure. Because of their speed, hash tables are very commonly used. 
+
+Hash Tables in the wild
+Pyhton has Dictionaries
+JS has Objects and Maps*
+Java, Go, & Scala hava Maps
+Ruby has Hashes
+
+    *Objects have some restrictions, but are basically hash tables
+*/
