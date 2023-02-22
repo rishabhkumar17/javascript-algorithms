e1fbcdb80bdb5001c9803fbffd399332986080f5
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-23 02:58:24 +0530

    Big O hash table

---
+++ Hash Tables/6_Big_O_Hash_Tables.js
@@ -0,0 +1,16 @@
+/* 
+Insert: O(1)
+Deletion: O(1)
+Access: O(1)
+*/
+
+/* 
+
+Hash tables are collection of key-value pair
+Hash tables can find values quickly given a key
+Hash tables can add new key-values quickly
+Hash tables store data in a large array, and work by hashing the keys
+A good hash should be fast, destribute keys uniformly, and be deterministic
+Separate chaining and linear probinf are two strategies used to deal
+    with two keys that hash to the same index
+*/
