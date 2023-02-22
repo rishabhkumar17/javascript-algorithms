6fa80e6a9d57984d77ae043866276cd880144818
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-23 02:46:35 +0530

    hash table keys

--- Hash Tables/5_Hash_Table_keys_values.js
+++ Hash Tables/5_Hash_Table_keys_values.js
@@ -8,6 +8,18 @@ Loops through the hash table array and
 returns an array of values in the table
 */
 
+keys() {
+    let keysArr = [];
+    for(let i=0; i<this.keyMap.length; i++) {
+        if(this.keyMap[i]) {
+            for(let j=0; j<this.keyMap[i].length; j++) {
+                keysArr.push(this.keyMap[i][j][0]);    
+            }
+        }
+    }
+    return keysArr;
+}
+
 values() {
     let valuesArr = [];
     for(let i=0; i<this.keyMap.length; i++) {
