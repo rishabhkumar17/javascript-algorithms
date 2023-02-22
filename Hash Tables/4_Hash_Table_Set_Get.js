f40aa1cff23cb7ad9e340e678692d80e5ecd439c
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-22 06:59:30 +0530

    set & get methods

--- Hash Tables/4_Hash_Table_Set_Get.js
+++ Hash Tables/4_Hash_Table_Set_Get.js
@@ -1,1 +1,33 @@
 // A Hash Table Class
+
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+}
+
+/* 
+    set
+Accepts a key and a value
+Hashes the key
+Stores the key-value pair in the hash table array via separate chaining
+*/
+
+/* 
+    get
+Accepts a key
+hashes the key
+Retieves the key-value pair in the hash table
+If the key is not found, returns undefined
+*/
